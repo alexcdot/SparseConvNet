31733befb4d49693de21647b796e335c9d8ed243
Yongkyun (Daniel) Lee <ylee2@titan.caltech.edu>
2021-05-20 19:08:51 -0700

    Simple example using max pooling

---
+++ examples/max_pooling.py
@@ -0,0 +1,31 @@
+import torch
+import sparseconvnet as scn
+
+if __name__ == '__main__':
+    use_cuda = torch.cuda.is_available() and scn.SCN.is_cuda_build()
+    device = 'cuda:0' if use_cuda else 'cpu'
+
+    pooling_layer = scn.Sequential(
+        scn.MaxPooling(2, 3, 2).to(device),
+        scn.SparseToDense(2, 1)
+    )
+
+    # expected output is [["10", "01"], ["11", "10"]]
+    data_arr = [["X    ", "     ", "     ", "     ", "   X ", "     "],
+                ["    X", "     ", "X    ", "     ", "     ", "     "]]
+
+    locations, features = [], []
+    for batchIdx, data in enumerate(data_arr):
+        for y, line in enumerate(data):
+            for x, c in enumerate(line):
+                if c == 'X':
+                    locations.append([y, x, batchIdx])
+                    features.append([1])
+    locations = torch.LongTensor(locations)
+    features = torch.FloatTensor(features).to(device)
+
+    inputSpatialSize = pooling_layer.input_spatial_size(torch.LongTensor([2, 2]))
+    input_layer = scn.InputLayer(2, inputSpatialSize)
+    input = input_layer([locations,features])
+    output = pooling_layer(input)
+    print(output)
